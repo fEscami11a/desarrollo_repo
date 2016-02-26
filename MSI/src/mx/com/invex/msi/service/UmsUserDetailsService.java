75e30aa8e988b2da3c5406ad1a8c89d82763e5e2
fEscami11a <fescamilla@invex.com>
2016-02-26 13:13:37 -0600

    fg

---
+++ MSI/src/mx/com/invex/msi/service/UmsUserDetailsService.java
@@ -0,0 +1,186 @@
+package mx.com.invex.msi.service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UmsUserDetailsService implements UserDetailsService
+{
+    public static final String ROLE_USER = "ROLE_USER";
+    public static final String ROLE_ADMINISTRATOR = "ROLE_ADMIN";
+    /** The JNDI name of the DataSource to use */
+	   protected String dsJndiName;
+	 
+	   /** The sql query to obtain the user roles */
+	   protected String rolesQuery;
+	   
+	   
+
+    private Logger logger = LoggerFactory.getLogger(UmsUserDetailsService.class);
+
+   
+
+  
+
+    public String getDsJndiName() {
+		return dsJndiName;
+	}
+
+	public void setDsJndiName(String dsJndiName) {
+		this.dsJndiName = dsJndiName;
+	}
+
+
+	public String getRolesQuery() {
+		return rolesQuery;
+	}
+
+	public void setRolesQuery(String rolesQuery) {
+		this.rolesQuery = rolesQuery;
+	}
+
+	
+
+  
+  
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
+    {
+        logger.debug("Loading user by name: " + username);
+        
+        if(username== null || username.trim().isEmpty()){
+        	throw new UsernameNotFoundException("Username " + username + " not found!");
+        }
+              Connection conn = null;
+ 	     
+ 	      PreparedStatement ps = null;
+ 	      ResultSet rs = null;
+ 	     try
+  	      {
+        InitialContext ctx = new InitialContext();
+	         DataSource ds = (DataSource) ctx.lookup(dsJndiName);
+	         conn = ds.getConnection();
+	         // Get the user role names
+	         ps = conn.prepareStatement(rolesQuery);
+	         try
+	         {
+	            ps.setString(1, username);
+	         }
+	         catch(ArrayIndexOutOfBoundsException ignore)
+	         {
+	            // The query may not have any parameters so just try it
+	         }
+	         rs = ps.executeQuery();
+	         List<GrantedAuthority> userAuthorities = new ArrayList<GrantedAuthority>();
+	         while(rs.next()){
+	        	 String role=rs.getString(1);
+	        	 logger.debug("role "+role);
+	        	 userAuthorities.add(new GrantedAuthorityImpl(role));
+	         }
+	         
+	         
+	         User user = new User(username, "password", true, true,
+	                    true, true, userAuthorities);
+	           
+	            return user;
+	         
+	         
+  	    }
+  	      catch(Exception ex)
+  	      {
+  	        logger.error("SQL failure", ex);
+  	       
+  	      }
+  	      finally
+  	      {
+  	         if( rs != null )
+  	         {
+  	            try
+  	            {
+  	               rs.close();
+  	            }
+  	            catch(SQLException e)
+  	            {}
+  	         }
+  	         if( ps != null )
+  	         {
+  	            try
+  	            {
+  	               ps.close();
+  	            }
+  	            catch(SQLException e)
+  	            {}
+  	         }
+  	         if( conn != null )
+  	         {
+  	            try
+  	            {
+  	               conn.close();
+  	            }
+  	            catch (Exception ex)
+  	            {}
+  	         }
+  	      }
+        
+//        if (username != null && username.equals("user"))
+//        {
+//            Vector<GrantedAuthority> userAuthorities = new Vector<GrantedAuthority>();
+//            userAuthorities.add(new GrantedAuthorityImpl(ROLE_USER));
+////            User user = new User("user", "Et6pb+wgWTVmq3VpLJlJWWgzrck=" /* SHA-1 encoded of "user" */, true, true,
+////                    true, true, userAuthorities.toArray(new GrantedAuthority[0]));
+//            User user = new User("user", getPassword(), true, true,
+//                  true, true, userAuthorities.toArray(new GrantedAuthority[0]));
+//          
+//            currentUser.set(user);
+//            return user;
+//        }
+//        if (username != null && username.equals("admin"))
+//        {
+//            Vector<GrantedAuthority> userAuthorities = new Vector<GrantedAuthority>();
+//            userAuthorities.add(new GrantedAuthorityImpl(ROLE_USER));
+//            userAuthorities.add(new GrantedAuthorityImpl(ROLE_ADMINISTRATOR));
+////            User user = new User("admin", "0DPiKuNIrrVmD8IUCuw1hQxNqZc=" /* SHA-1 encoded of "admin" */, true, true,
+////                    true, true, userAuthorities.toArray(new GrantedAuthority[0]));
+//            User user = new User("admin", getPassword(), true, true,
+//                    true, true, userAuthorities.toArray(new GrantedAuthority[0]));
+//            currentUser.set(user);
+//            return user;
+//        }
+        throw new UsernameNotFoundException("Username " + username + " not found!");
+    }
+
+//    public User getCurrentUser()
+//    {
+//        return currentUser.get();
+//    }
+//
+//    public void setCurrentUser(User user)
+//    {
+//        currentUser.set(user);
+//    }
+//
+//    public String getPassword()
+//    {
+//        return currentPassword.get();
+//    }
+//
+//    public void setPassword(String password)
+//    {
+//        currentPassword.set(password);
+//    }
+}
---
+++ MSI/src/mx/com/invex/msi/ws/InfoCuentaDto.java
@@ -0,0 +1,139 @@
+package mx.com.invex.msi.ws;
+
+public class InfoCuentaDto {
+private String cuenta;
+private String nombreCompleto;
+private String fechaNacimiento;
+private String telCel;
+private String rfc;
+private String email;
+private String calleNum;
+private String colonia;
+private String munEdo;
+private String codigoPostal;
+private String saldoAlDia;
+private String fechaLimitePago;
+private String pagoMinimo;
+private String creditLimit;
+private String creditoDisponible;
+private String pagoNoIntereses;
+private String tpc,cpc;
+private int cycle;
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getCuenta() {
+	return cuenta;
+}
+public void setCuenta(String cuenta) {
+	this.cuenta = cuenta;
+}
+public String getNombreCompleto() {
+	return nombreCompleto;
+}
+public void setNombreCompleto(String nombreCompleto) {
+	this.nombreCompleto = nombreCompleto;
+}
+public String getFechaNacimiento() {
+	return fechaNacimiento;
+}
+public void setFechaNacimiento(String fechaNacimiento) {
+	this.fechaNacimiento = fechaNacimiento;
+}
+public String getTelCel() {
+	return telCel;
+}
+public void setTelCel(String telCel) {
+	this.telCel = telCel;
+}
+public String getRfc() {
+	return rfc;
+}
+public void setRfc(String rfc) {
+	this.rfc = rfc;
+}
+public String getCalleNum() {
+	return calleNum;
+}
+public void setCalleNum(String calleNum) {
+	this.calleNum = calleNum;
+}
+public String getColonia() {
+	return colonia;
+}
+public void setColonia(String colonia) {
+	this.colonia = colonia;
+}
+public String getMunEdo() {
+	return munEdo;
+}
+public void setMunEdo(String munEdo) {
+	this.munEdo = munEdo;
+}
+public String getCodigoPostal() {
+	return codigoPostal;
+}
+public void setCodigoPostal(String codigoPostal) {
+	this.codigoPostal = codigoPostal;
+}
+public String getSaldoAlDia() {
+	return saldoAlDia;
+}
+public void setSaldoAlDia(String saldoAlDia) {
+	this.saldoAlDia = saldoAlDia;
+}
+public String getFechaLimitePago() {
+	return fechaLimitePago;
+}
+public void setFechaLimitePago(String fechaLimitePago) {
+	this.fechaLimitePago = fechaLimitePago;
+}
+public String getPagoMinimo() {
+	return pagoMinimo;
+}
+public void setPagoMinimo(String pagoMinimo) {
+	this.pagoMinimo = pagoMinimo;
+}
+public String getCreditLimit() {
+	return creditLimit;
+}
+public void setCreditLimit(String creditLimit) {
+	this.creditLimit = creditLimit;
+}
+public String getCreditoDisponible() {
+	return creditoDisponible;
+}
+public void setCreditoDisponible(String creditoDisponible) {
+	this.creditoDisponible = creditoDisponible;
+}
+public String getPagoNoIntereses() {
+	return pagoNoIntereses;
+}
+public void setPagoNoIntereses(String pagoNoIntereses) {
+	this.pagoNoIntereses = pagoNoIntereses;
+}
+public String getTpc() {
+	return tpc;
+}
+public void setTpc(String tpc) {
+	this.tpc = tpc;
+}
+public String getCpc() {
+	return cpc;
+}
+public void setCpc(String cpc) {
+	this.cpc = cpc;
+}
+public int getCycle() {
+	return cycle;
+}
+public void setCycle(int cycle) {
+	this.cycle = cycle;
+}
+
+
+
+}
